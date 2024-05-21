fb914f228bb0cff711e3e48dae9d1f3d13a60a9a
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-05-21 11:24:37 +0300

    Minor improvements

--- request_handler/index.ts
+++ request_handler/index.ts
@@ -4,16 +4,18 @@ import { connectToQueue } from "./utils/queue";
 const app = express();
 const port = 3000;
 
-app.post("/", async (req: any, res: any) => {
+app.post("/", async (req: express.Request, res: express.Response) => {
   try {
     const redisClient = await connectToQueue();
     await redisClient.set('request-handler', 'rh-value');
+
     const value = await redisClient.get('request-handler');
     await redisClient.quit(); // Properly close the client after operation
-    res.send(`Added entry to redis: ${value}`);
+
+    res.status(202).send(`Added entry to redis: ${value}`);
   } catch (error) {
-    console.error('Error interacting with Redis:', error);
-    res.status(500).send('Failed to interact with Redis');
+    console.error('Error to interact with queuing service:', error);
+    res.status(500).send('Failed to interact with queuing service.');
   }
 });
 
