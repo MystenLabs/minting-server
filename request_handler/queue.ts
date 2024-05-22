26ef6e6c9eebc58e815596f2629430533b78f824
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-05-22 12:15:30 +0300

    Replace Bull with BullMQ
    BullMQ is a fork of Bull, with more features
    and provides a better interface for
    building multiple workers.

--- request_handler/queue.ts
+++ request_handler/queue.ts
@@ -1,10 +1,5 @@
-import Bull from "bull";
-
-export type QueueObject = {
-  id: String;
-  requestorAddress: String;
-  type: String;
-};
+import { Queue, Worker } from "bullmq";
+import { generatePID } from "./utils/pid";
 
 // TODO: Parse host & port from `.env`
 const redisConfig = {
@@ -14,26 +9,52 @@ const redisConfig = {
   // password: TODO
 };
 
-const requestsQueue = new Bull("requests-queue", {
-  redis: redisConfig,
-  // Rate limiting to X requests (max) per Y seconds (duration).
-  limiter: {
-    max: 1000, // TODO: move to .env
-    duration: 5000, // TODO: move to .env
-  },
+export type QueueObject = {
+  id: String;
+  requestorAddress: String;
+  type: String;
+};
+
+const requestsQueue = new Queue("requests-queue", {
+  connection: redisConfig,
 });
 
-// Requests are automatically dequeued.
-// Here is defined how each request should be processed.
-requestsQueue.process(async (job, done) => {
-  console.log("Inside job process");
-  job.progress(0);
-  await new Promise((resolve) => setTimeout(resolve, 4000));
-  job.progress(100);
-  done();
-});
+// Create 5 workers
+for (let i = 0; i < 5; i++) {
+  /*
+  When a worker instance is created, it launches the processor immediately
+  */
+  new Worker(
+    "requests-queue",
+    async (job) => {
+      console.log(`Processing ${job.data.id} - ts: ` + Date.now());
+      job.updateProgress(0);
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      job.updateProgress(25);
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      job.updateProgress(50);
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      job.updateProgress(75);
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      job.updateProgress(100);
+    },
+    {
+      connection: redisConfig,
+      /*
+      # REGARDING CONCURRENCY
+      Even when having multiple workers in different machines, choose a high concurrency
+      factor for every worker, so that the resources of every machine where the worker
+      is running are used more efficiently.
+      */
+      concurrency: 200, // number of jobs a single worker can do in parallel
+    },
+  );
+}
 
 /// Push a request to the queue in order to place it for consumption.
 export async function enqueRequest(queueObject: QueueObject) {
-  await requestsQueue.add(queueObject);
+  await requestsQueue.add(
+    `job-${generatePID()}`, // job name
+    queueObject, // payload
+  );
 }
