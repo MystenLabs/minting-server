5431db29dc3587987363df5ecf3ae9c21c8cc32f
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-06-04 15:18:11 +0300

    Add test for POST to "/"

--- request_handler/tests/handle-requests.test.ts
+++ request_handler/tests/handle-requests.test.ts
@@ -9,3 +9,15 @@ test("Healthcheck endpoint returns status 200", async () => {
       expect(response.statusCode).toBe(200);
     })
 })
+
+test("Accept request for processing", async () => {
+  await request(app)
+    .post("/")
+    .send({
+      "address": "0xe40c8cf8b53822829b3a6dc9aea84b62653f60b771e9da4bd4e214cae851b87b",
+      "type": "mint"
+    })
+    .then(response => {
+      expect(response.statusCode).toBe(202);
+    })
+})
