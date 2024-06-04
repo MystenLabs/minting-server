bc1d37df40e85ee87c933c10df02e0a131733e0c
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-06-04 14:21:58 +0300

    Fix REDIS_PORT env var parsing

--- request_handler/queue.ts
+++ request_handler/queue.ts
@@ -3,7 +3,7 @@ import { generatePID } from "./utils/pid";
 
 const redisConfig = {
   host: process.env.REDIS_HOST ?? "127.0.0.1",
-  port: Number(process.env.REDIS_PORT) ?? 6379,
+  port: Number(process.env.REDIS_PORT ?? 6379),
   // TODO username: process.env.REDIS_USER,
   // TODO password: process.env.REDIS_PASSWORD,
 };
