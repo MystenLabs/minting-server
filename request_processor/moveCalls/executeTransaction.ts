37bb97b800ce2e6dc58c6bf9fea1b576695f6ebc
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-06-10 13:02:42 +0300

    Add ParallelTransactionExecutor

--- request_processor/moveCalls/executeTransaction.ts
+++ request_processor/moveCalls/executeTransaction.ts
@@ -4,6 +4,7 @@ import { SuiClient, getFullnodeUrl } from "@mysten/sui/client";
 import { ADMIN_SECRET_KEY } from "../utils/config";
 import { prepareTransaction} from "./prepareTransaction";
 import { Job } from "bullmq";
+import { ParallelTransactionExecutor } from "@mysten/sui/transactions";
 
 
 const suiClient = new SuiClient({url: getFullnodeUrl('testnet')});
@@ -16,19 +17,20 @@ const adminKeypair = Ed25519Keypair.fromSecretKey(
     adminPrivateKeyArray.slice(1)
 );
 
-const adminAddress = adminKeypair.getPublicKey().toSuiAddress();
+const executor = new ParallelTransactionExecutor({
+  client: suiClient,
+  signer: adminKeypair,
+
+  // The maximum number of gas coins to keep in the gas pool,
+  // which also limits the maximum number of concurrent transactions
+  maxPoolSize: 50,
+})
 
 export async function executeTransaction(receivers: string[], job: Job) {
     const transaction = await prepareTransaction(receivers)
     job.updateProgress(50);
-    const res = await suiClient.signAndExecuteTransaction({
-        transaction: transaction,
-        signer: adminKeypair,
-        requestType: "WaitForLocalExecution",
-        options: {
-            showEffects: true
-        }
-    });
 
-    return {status: res.effects?.status.status, digest: res.digest};
+    const res = await executor.executeTransaction(transaction)
+
+    return {status: res.effects, digest: res.digest};
 }
