13c6b8163a6848d548a7cddf41383c39b707d53a
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-06-13 19:06:09 +0300

    Move bullmq worker concurrency to environment variables

--- request_processor/index.ts
+++ request_processor/index.ts
@@ -8,26 +8,26 @@ const redisConfig = {
 };
 
 const socket = new WebSocket(`ws://notifier:3001`);
-console.log('Connected to notifier websocket server');
-console.log(socket)
+console.log("Connected to notifier websocket server");
+console.log(socket);
 
 const worker = new Worker(
   "requests-queue",
   async (job) => {
-      try {
-        job.updateProgress(10)
-        console.log(`Executing transactions in bulk: ${job.data}`)
-        const resp = await executeTransaction(job.data);
-        job.updateProgress(90)
-        if (resp.status === 'failure') {
-          throw new Error(`Transaction failed: ${resp.status}`);
-        }
-        job.updateProgress(100);
-        return { jobId: job.id, result: resp.status, digest: resp.digest };
-      } catch (e) {
-        console.error(`Error executing bulk of transactions: ${job.data} - `, e);
-        throw e;
+    try {
+      job.updateProgress(10);
+      console.log(`Executing transactions in bulk: ${job.data}`);
+      const resp = await executeTransaction(job.data);
+      job.updateProgress(90);
+      if (resp.status === "failure") {
+        throw new Error(`Transaction failed: ${resp.status}`);
       }
+      job.updateProgress(100);
+      return { jobId: job.id, result: resp.status, digest: resp.digest };
+    } catch (e) {
+      console.error(`Error executing bulk of transactions: ${job.data} - `, e);
+      throw e;
+    }
   },
   {
     name: `worker-${process.env.HOSTNAME ? "container-" + process.env.HOSTNAME : "localhost"}`,
@@ -38,21 +38,23 @@ const worker = new Worker(
     factor for every worker, so that the resources of every machine where the worker
     is running are used more efficiently.
     */
-    concurrency: 10,
+    concurrency: process.env.BULLMQ_WORKER_CONCURRENCY
+      ? parseInt(process.env.BULLMQ_WORKER_CONCURRENCY)
+      : 10,
   },
 );
 
-worker.on('completed', async (job: Job) => {
+worker.on("completed", async (job: Job) => {
   // Send a message to the notifier server
-  socket.send(JSON.stringify(
-    {
+  socket.send(
+    JSON.stringify({
       jobData: job.data,
-      returnValue: job.returnvalue
-    }
-  ));
-})
+      returnValue: job.returnvalue,
+    }),
+  );
+});
 
-worker.on('failed', (job?: Job, err?: Error, prev?: string) => {
+worker.on("failed", (job?: Job, err?: Error, prev?: string) => {
   if (!job || !err) {
     console.error(`Failed job is undefined or error is undefined`);
     return;
@@ -60,16 +62,17 @@ worker.on('failed', (job?: Job, err?: Error, prev?: string) => {
 
   console.error(`Job failed: ${job.id} with error:`, err);
 
-  socket.send(JSON.stringify({
-    jobData: job.data,
-    error: err.message,
-  }));
+  socket.send(
+    JSON.stringify({
+      jobData: job.data,
+      error: err.message,
+    }),
+  );
 
   // TODO add retry
 });
 
-
-worker.on('error', err => {
+worker.on("error", (err) => {
   // [WARNING] If the error handler is missing, the worker may stop processing jobs when an error is emitted!
   console.error("Worker error:", err);
 });
