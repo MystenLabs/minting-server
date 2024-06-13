7ae23473ceaa45f0b2da8b8805fa4324b2afed6d
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-06-13 19:16:24 +0300

    Remove unneeded test for processor

--- request_processor/tests/config.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "bun:test";
-import { getSmartContractFunctionsConfig }  from "../utils/config";
-
-test("smart_contract_config.yaml gets serialized successfully", async () => {
-  const contractSetup = await getSmartContractFunctionsConfig();
-  expect(contractSetup).toBeDefined();
-});
