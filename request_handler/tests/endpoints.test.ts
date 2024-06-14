a92e89cc384ca38f845e89fadbc803e2e48548b5
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-06-14 14:21:21 +0300

    Update request_handler tests

--- request_handler/tests/endpoints.test.ts
+++ request_handler/tests/endpoints.test.ts
@@ -14,8 +14,9 @@ test("Accept request for processing", async () => {
   await request(app)
     .post("/")
     .send({
-      "address": "0xe40c8cf8b53822829b3a6dc9aea84b62653f60b771e9da4bd4e214cae851b87b",
-      "type": "mint"
+      "smart_contract_function_name": "mint_nft",
+	    "smart_contract_function_arguments": ["0x9320eaaf945570b1baf7607f98a9cf5585fdcb8ed09d46da93199fee16b48196"],
+			"receiver_address": "0xe40c8cf8b53822829b3a6dc9aea84b62653f60b771e9da4bd4e214cae851b87b"
     })
     .then(response => {
       expect(response.statusCode).toBe(202);
