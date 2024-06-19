3abf63c222b2f64cbfa7e022f16c7b3e259b1961
Maria Siopi <maria@mystenlabs.com>
2024-06-19 18:37:05 +0300

    prettier

--- load_testing/mint.js
+++ load_testing/mint.js
@@ -2,15 +2,16 @@ import http from "k6/http";
 import { Trend } from "k6/metrics";
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
-const responseTimeTrend = new Trend('response_time');
+const responseTimeTrend = new Trend("response_time");
 
 export const options = {
   iterations: 5000,
 };
 
 export function handleSummary(data) {
-  
-  console.log(`Average response time: ${data.metrics['response_time'].values['p(95)']} ms`);
+  console.log(
+    `Average response time: ${data.metrics["response_time"].values["p(95)"]} ms`
+  );
   return {
     "summary.html": htmlReport(data),
   };
@@ -20,8 +21,11 @@ export default function () {
   const url = "http://localhost:3000/";
   const payload = JSON.stringify({
     smartContractFunctionName: "mint_nft",
-    smartContractFunctionArguments: ["0xc44fb22cb1786b4eae31ada831bb0fa2549612ea1dabec5231a792c9fa921f46"],
-    receiverAddress: "0xe40c8cf8b53822829b3a6dc9aea84b62653f60b771e9da4bd4e214cae851b87b",
+    smartContractFunctionArguments: [
+      "0xc44fb22cb1786b4eae31ada831bb0fa2549612ea1dabec5231a792c9fa921f46",
+    ],
+    receiverAddress:
+      "0xe40c8cf8b53822829b3a6dc9aea84b62653f60b771e9da4bd4e214cae851b87b",
   });
 
   const params = {
--- load_testing/stats.ts
+++ load_testing/stats.ts
@@ -1,37 +1,37 @@
-import Redis from 'ioredis';
-import { readFileSync } from 'fs';
+import Redis from "ioredis";
+import { readFileSync } from "fs";
 
 const redis = new Redis({
-    host: process.env.REDIS_HOST ?? "127.0.0.1",
-    port: Number(process.env.REDIS_PORT ?? 6379),
-    password: process.env.REDIS_PASSWORD,
-  });
+  host: process.env.REDIS_HOST ?? "127.0.0.1",
+  port: Number(process.env.REDIS_PORT ?? 6379),
+  password: process.env.REDIS_PASSWORD,
+});
 
 async function calculateAverages() {
-    try {
-        const avgLuaScript = readFileSync('calculate_averages.lua', 'utf8');
-        const medianLuaScript = readFileSync('median_response_time.lua', 'utf8');
-        const avgResult: any = await redis.eval(avgLuaScript, 0);
-        const medianResult: any = await redis.eval(medianLuaScript, 0);
-        const avgFinishedTime = avgResult;
-        const medianResponseTime = medianResult;
-        console.log(`Average Finished Time: ${avgFinishedTime} ms`);
-        console.log(`Median Response Time: ${medianResponseTime} ms`);
-      } catch (error) {
-        console.error('Error running Lua script:', error);
-      }
-} 
-
-async function main() {
-    try {
-        await calculateAverages();
-      } catch (error) {
-        console.error('Error in main function:', error);
-      } finally {
-        redis.disconnect();
-      }
+  try {
+    const avgLuaScript = readFileSync("calculate_averages.lua", "utf8");
+    const medianLuaScript = readFileSync("median_response_time.lua", "utf8");
+    const avgResult: any = await redis.eval(avgLuaScript, 0);
+    const medianResult: any = await redis.eval(medianLuaScript, 0);
+    const avgFinishedTime = avgResult;
+    const medianResponseTime = medianResult;
+    console.log(`Average Finished Time: ${avgFinishedTime} ms`);
+    console.log(`Median Response Time: ${medianResponseTime} ms`);
+  } catch (error) {
+    console.error("Error running Lua script:", error);
+  }
 }
 
-main().catch(err => {
-    console.log('Error:', err);
+async function main() {
+  try {
+    await calculateAverages();
+  } catch (error) {
+    console.error("Error in main function:", error);
+  } finally {
+    redis.disconnect();
+  }
+}
+
+main().catch((err) => {
+  console.log("Error:", err);
 });
