40d9bb137d0b81931b337f88f7193a62637e816b
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-06-25 11:44:33 +0300

    Update mint.js params

--- load_testing/mint.js
+++ load_testing/mint.js
@@ -17,11 +17,11 @@ export const options = {
   scenarios: {
     constant_request_rate: {
       executor: "constant-arrival-rate",
-      rate: 400,
+      rate: 300,
       timeUnit: "1s",
-      duration: "1m",
-      preAllocatedVUs: 20,
-      maxVUs: 100,
+      duration: "10m",
+      preAllocatedVUs: 10,
+      maxVUs: 20,
     },
   },
 };
@@ -30,7 +30,9 @@ export default function () {
   const url = "http://localhost:3000/";
   const payload = JSON.stringify({
     smartContractFunctionName: "mint_nft",
-    smartContractFunctionArguments: [process.env.ADMIN_CAP],
+    smartContractFunctionArguments: [
+      "0xdd33675337d769bc9cc4120e204afd8e3f6aa047b2a9ee5d6c6c1dcbc87bd169",
+    ],
     receiverAddress:
       "0x021318ee34c902120d579d3ed1c0a8e4109e67d386a97b841800b0a9763553ef",
   });
